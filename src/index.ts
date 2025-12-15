73f3de1f76021fb37fe43e28184ecef52e0b0e0a
Gabriel Wolfgang <bielstephane9722@gmail.com>
2025-12-15 15:19:02 -0300

    teste

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("oi");
+function repeat<T>(qntd: number, action: () => void) {
+  let i = 0;
+  while (i < qntd) {
+    action();
+    i++;
+  }
+}
+export function log(msg: string, qntd: number): void {
+  repeat(qntd, () => console.log(msg));
+}
+log("gabriel", 5);
